587d809e552fac1bae19234ac4f3c95b59dd870a
Danielle Laforte <daniellelaforte@Danielles-MacBook-Pro-2.local>
2015-09-06 20:31:52 -0600

    fixed the infinite loop by type casting to Number

--- Volunteers.js
+++ Volunteers.js
@@ -1,29 +1,41 @@
+var namearr = [];
+var phonearr =[];
+var streetarr =[];
+var namearrvo = [];
+var phonearrvo =[];
+var streetarrvo =[];
 var name;
 var phone;
 var street;
+var namevo;
+var phonevo;
+var streetvo;
 
 var victims = prompt('How many disaster victims do you wish to enter?');
 
-for (var i=1; i<victims + 1; i++) {
-	var name = prompt('Please enter the name of Victim ' + i + '.');
+victims = Number(victims);
 
-	var phone = prompt('Please enter the phone number of Victim ' + i + '.');
 
-	var street = prompt('Please enter the street address of Victim ' + i + '.');
-	name.push(name);
-	phone.push(phone);
-	street.push(street); }
+for (var i = 1; i < (victims + 1); i++) {
+	name = prompt('Please enter the name of Victim ' + i + '.'); 
+    namearr.push(name);
+	phone = prompt('Please enter the phone number of Victim ' + i + '.');
+    phonearr.push(phone);
+	street = prompt('Please enter the street address of Victim ' + i + '.');
+	streetarr.push(street);
+	}
 
 var volunteers = prompt('How many disaster volunteers do you wish to enter?');	
 
-for (var i=1; i<volunteers + 1; i++) {
-	var name = prompt('Please enter the name of Volunteer ' + i + '.');
+volunteers = Number(volunteers);
 
-	var phone = prompt('Please enter the phone number of Volunteer ' + i + '.');
-
-	var street = prompt('Please enter the street address of Volunteer ' + i + '.');
-	name.push(name);
-	phone.push(phone);
-	street.push(street); }
+for (var j=1; j< (volunteers + 1); j++) {
+	namevo = prompt('Please enter the name of Volunteer ' + j + '.');
+    namearrvo.push(namevo);
+	phonevo = prompt('Please enter the phone number of Volunteer ' + j + '.');
+    phonearrvo.push(phonevo);
+	streetvo = prompt('Please enter the street address of Volunteer ' + j + '.');
+	streetarrvo.push(streetvo);
+	}
 
 
